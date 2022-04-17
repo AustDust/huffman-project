5d8052bd1091495d7263b3df168ce3e4aad38618
AustDust <75823985+AustDust@users.noreply.github.com>
2022-04-17 00:00:58 -0500

    Task 2 and 3

--- huffmanTree.cpp
+++ huffmanTree.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+#include <map>
+#include <fstream>
 using namespace std;
+const int MAX = 128;
 
 
 class huffmanType
@@ -14,7 +17,7 @@ class huffmanType
 
             nodeType()
             {
-                ch = '\0';
+                ch = '*';
                 weight = 0;
                 next = NULL;
                 zero = NULL;
@@ -37,9 +40,135 @@ class huffmanType
         void altprintHuffTree(nodeType *p, int lvl) {
             if(p) {
                 cout << p->ch << " " << p->weight << " " << lvl << "\n";
-                altprintHuffTree(p->zero, ++lvl);
-                altprintHuffTree(p->one, ++lvl);
+                altprintHuffTree(p->zero, lvl+1);
+                altprintHuffTree(p->one, lvl+1);
             }
         }
 
+        bool isThere(nodeType *p, char cr) {
+            if(!p) {
+                return false;
+            }
+            else {
+                if(!(p)->zero && p->ch == cr) {
+                    return true;
+                }
+                else {
+                    return isThere(p->zero, cr) || isThere(p->one, cr);
+                }
+            }
+        }
+    public:
+        huffmanType() {
+            root = NULL;
+            envPtr = new nodeType*[MAX];
+            for (int i = 0; i < MAX; ++i) {
+                envPtr[i] = NULL;
+            }
+        }
+        
+        void loadArray(map<char, int> a) {
+            for(auto it = a.begin(); it != a.end(); ++it) {
+                int location = (int)it->first;
+                envPtr[location] = new nodeType;
+                envPtr[location]->ch = it->first;
+                envPtr[location]->weight = it->second;
+            }
+        }
+
+        void buildLinkedList() {
+            for(int i = 0; i < MAX; ++i) {
+                if(envPtr[i]) {
+                    put(envPtr[i]);
+                }
+            }
+        }
+
+        void printLinkedList() {
+            if(!root) {
+                cout << "Linked List Empty" << endl;
+                return;
+            }
+            else {
+                nodeType *p;
+                p = root->next;
+                cout << root->ch << "\t" << root->weight << "\n";
+                while (p) {
+                    cout << p->ch << "\t" << p->weight << "\n";
+                    p = p->next;
+                }
+            }
+
+        }
+
+        void buildhuffManTree() {
+            nodeType *p;
+            while(root->next) {
+                p = new nodeType;
+                p->zero = root;
+                p->one = root->next;
+                p->weight = p->zero->weight + p->one->weight;
+                root = root->next->next;
+                put(p);
+            }
+        }
+
+        void printHuffTree() {
+            altprintHuffTree(root, 0);
+        }
+
 };
+
+void printMap(map<char, int> m)
+{
+    for(auto it = m.begin(); it != m.end(); ++it)
+    {
+        cout << it->first << "\t" << it->second << endl;
+    }
+}
+
+int main()
+{
+    /***** TASK ONE- INPUT *****/
+    /* OPen file stream */
+    ifstream in;
+    in.open("input.txt");
+    if(!in)
+    {
+        cout << "ERR: File not found" << endl;
+        return 1;
+    }
+    
+    /* Use a map to count characters */
+    char c;
+    map<char, int> charMap;
+    while(!in.eof())
+    {
+        in >> c;
+        if(in.eof())
+            break;
+
+        if(charMap.find(c) == charMap.end())
+            charMap.emplace(c, 1);
+        else
+            charMap.at(c)++;
+    }
+
+    cout << "\n\nPrinting Characters In File\n\n";
+    printMap(charMap);
+    in.close();
+
+    huffmanType test;
+    test.loadArray(charMap);
+    test.buildLinkedList();
+
+    cout << "\n\nPrinting Linked List\n\n";
+    test.printLinkedList();
+
+    test.buildhuffManTree();
+
+    cout << "\n\nPrinting Huffman Tree\n\n";
+    test.printHuffTree();
+
+    return 0;
+}
